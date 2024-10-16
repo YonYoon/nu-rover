04a12c37ee4375694ce17f965ecc0a5bae95e839
YonYoon <zhzhzh1806@gmail.com>
2024-10-16 21:56:48 +0500

    feat: implement table of data and secret

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// link - https://jsonplaceholder.typicode.com/comments
-
-class Post {
-    constructor(postId, id, name, email, body) {
-        this.postId = postId;
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.body = body;
-    }
-}
-
 async function fetchData() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/comments");
@@ -24,23 +12,66 @@ async function fetchData() {
 }
 
 function displayData(data) {
-    const container = document.getElementById('table');
+    const container = document.getElementById('data');
     container.innerHTML = '';
 
+    const table = document.createElement('table');
+    container.appendChild(table);
+
     data.forEach(item => {
-        const div = document.createElement('div');
-        div.className = "item";
+        for (const key in item) {
+            if (key === "postId") {
+                continue;
+            }
+            
+            const row = document.createElement("tr");
 
-        div.innerHTML = `
-            <p>postId: ${item.postId}<p>
-            <p>id: ${item.id}<p>
-            <p>name: ${item.name}<p>
-            <p>email: ${item.email}<p>
-            <p>body: ${item.body}<p>
-        `;
+            const th = document.createElement("th");
+            th.textContent = key;
+            row.appendChild(th);
 
-        container.appendChild(div);
+            const td = document.createElement("td");
+            if (key === "id") {
+                th.classList.add('id-row');
+                td.classList.add('id-row');
+                td.textContent = `${item["postId"]}.${item[key]}`
+            } else {
+                td.textContent = item[key];
+            }
+            row.appendChild(td);
+
+            table.appendChild(row);
+        }
+    })
+
+    showSecret();
+}
+
+function showSecret() {
+    const secret = document.getElementById("secret");
+
+    const question = document.createElement("h1");
+    question.innerText = "Want to support your mission?"
+    secret.appendChild(question);
+
+    const span = document.createElement("span");
+    span.innerText = "By inviting me to your project,";
+    span.classList.add("highlight");
+
+    const text = document.createElement("h2");
+    text.appendChild(span);
+    text.append(" you can directly contribute to the progress and success of this groundbreaking mission. Your kindness will let me to help you to: develop software, have fun, and win competitions");
+    secret.appendChild(text);
+
+    const button = document.createElement("button");
+    button.textContent = "Support now";
+    button.addEventListener("click", function () {
+        window.localStorage.href = "https://t.me/yonyoon";
     });
+    button.classList.add("support-button");
+
+    secret.classList.add("center-div");
+    secret.appendChild(button);
 }
 
 window.onload = fetchData;
